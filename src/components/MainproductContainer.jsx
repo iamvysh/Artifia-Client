a4c470350bb74ade1ac3ab5dc14039b2ca975371
iamvysh <vyshnavthaithottathil@gmail.com>
2024-06-16 23:54:08 +0530

    navigation fixed

--- src/components/MainproductContainer.jsx
+++ src/components/MainproductContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box,styled } from '@mui/material'
 import { Data } from '../assets/Data';
 import { FaStar } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 
 
@@ -15,15 +16,15 @@ const Maincontainer = styled(Box)(() => ({
     padding:"1% 6%"
   }));
 const MainproductContainer = () => {
-
+    const navigate=useNavigate()
   return (
     <>
     <Maincontainer>
         <div className="container">
 
        <div className="row">
-       //map here
-        <div className="col-md-4" >
+       
+        <div className="col-md-4" onClick={()=>navigate("/productdetails")} >
             <div className="card-wrap" style={{padding:"10px",width:"100%",background:"white",border:"2px solid #B6B6B6",borderRadius:"20px"}}>
 
             <img src={Data[0].image} alt="" style={{width:"100%",objectFit:"cover"}}/>
@@ -39,39 +40,40 @@ const MainproductContainer = () => {
             </div>
             </div>
         </div>
-        <div className="col-md-4"> 
-        <div className="card-wrap" style={{padding:"10px",width:"100%",background:"white",border:"2px solid #B6B6B6",borderRadius:"20px"}}>
+        <div className="col-md-4" onClick={()=>navigate("/productdetails")} >
+            <div className="card-wrap" style={{padding:"10px",width:"100%",background:"white",border:"2px solid #B6B6B6",borderRadius:"20px"}}>
 
             <img src={Data[0].image} alt="" style={{width:"100%",objectFit:"cover"}}/>
-            <p>{Data[0].title}</p>
-            <p>{Data[0].price}</p>
+            <p style={{color:"#003F62",fontWeight:"600"}}>{Data[0].title}</p>
+            <p style={{color:"black",fontWeight:"700"}}>${Data[0].price}</p>
             <div className="star-container">
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
+          
             </div>
             </div>
-            
-            </div>
-        <div className="col-md-4">
-            
-        <div className="card-wrap" style={{padding:"10px",width:"100%",background:"white",border:"2px solid #B6B6B6",borderRadius:"20px"}}>
+        </div>
+        <div className="col-md-4" onClick={()=>navigate("/productdetails")} >
+            <div className="card-wrap" style={{padding:"10px",width:"100%",background:"white",border:"2px solid #B6B6B6",borderRadius:"20px"}}>
 
-<img src={Data[0].image} alt="" style={{width:"100%",objectFit:"cover"}}/>
-<p>{Data[0].title}</p>
-<p>{Data[0].price}</p>
-<div className="star-container">
-<FaStar style={{color:"#ACACAC"}} />
+            <img src={Data[0].image} alt="" style={{width:"100%",objectFit:"cover"}}/>
+            <p style={{color:"#003F62",fontWeight:"600"}}>{Data[0].title}</p>
+            <p style={{color:"black",fontWeight:"700"}}>${Data[0].price}</p>
+            <div className="star-container">
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
             <FaStar style={{color:"#ACACAC"}} />
-</div>
-</div>
-            
+            <FaStar style={{color:"#ACACAC"}} />
+          
             </div>
+            </div>
+        </div>
+        
+       
     
      </div>
         </div>
